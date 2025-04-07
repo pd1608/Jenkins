64f51d6547d2cbda69fbd10eb39d17e542548608
pd1608 <pranav.deepak1608@gmail.com>
2025-04-06 19:33:44 -0600

    new change4

--- test.py
+++ test.py
@@ -1,17 +1,5 @@
-from napalm import get_network_driver
-def ping_from_router():
-    driver = get_network_driver("ios")
-    router = driver(
-        '198.51.100.12',
-        'lab',
-        'lab123')
-    
-    router.open()
+def main():
 
-    result = router.ping('10.1.5.1')
-    router.close()
+    print("Hello!!!!!!!!")
 
-    return isinstance(result,dict)
-    
-        
-ping_from_router()
+main()
